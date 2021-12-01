afa4a0c56491dde5f917869089dd65955fdf8cf7
ssattu <satyam@devohack.com>
2021-12-01 10:56:56 +0530

    Create area_perimeter

---
+++ circle_area_peri.rb
@@ -0,0 +1,8 @@
+ puts "Enter the radius of circle: "
+ radius=gets.to_f
+ puts "The radius of the cirle is: #{radius}"
+
+ area =3.14*radius*radius
+ perimeter =2*3.14*radius
+ puts "The Arae The Circle is: #{area}"
+ puts "The Perimeter Of the circle is: #{perimeter}   "
