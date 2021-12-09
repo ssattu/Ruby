dd1bf9d5a3c1574ced549f8f60e6cd0cb6f73092
ssattu <satyam@devohack.com>
2021-12-09 18:17:19 +0530

    Add assignment1 of ruby

---
+++ Assignment1.rb
@@ -0,0 +1,75 @@
+#1.  Write Ruby Program to get ruby version with patch number.
+v = RUBY_VERSION
+puts v 
+p = RUBY_PATCHLEVEL
+puts p
+
+#2. Write a Ruby program to display current date and time.
+
+dt = Time.now
+puts "#{dt}"
+
+#3.Write a Ruby program to creata new string which is n copies of a given string where n is a non negative integer. 
+
+# puts "Enter alphabet of num you want to print"
+# v =  gets.chomp
+# puts "Enter the no. of rows"
+# num=gets.to_i
+# for i in (1..num) do
+#   p v*i
+# end
+
+#4. Write a Ruby program which axxept thr radiu od a circle from the user and compute the parameter and area.
+
+# puts "Enter the Radius of the circle in cm."
+# r = gets.chomp.to_f
+# area= 3.14*r*r
+# parameter= 3.14*2*r
+
+# puts "The Area of the Circle is: #{area} sq.cm"
+# puts "The Parameter of the circle: #{parameter} cm"
+
+#5. Write a Ruby program to check whether a string starts with "if"
+
+ 
+
+#6. Write a Ruby program which accept the user's first and last name and print them in reverse order with a space between them.
+
+# puts "Enter Your first name: "
+# first=gets.chomp
+# puts "Enter Your last name: "
+# last=gets.chomp
+
+# puts "Hello #{last} #{first}"
+
+#7. Write a Ruby program to accept a filename from the user print the extension of that.
+
+# puts "Enter any file name with full path"
+# file=gets.chomp
+# ex= file.match(/.\w+$/)
+#  puts "Extention: #{ex}"
+
+#Write a Ruby program which accept the user's first and last name and print them in reverse order with a space between them.
+
+# puts "Enter Your first name: "
+# first=gets.chomp
+# puts "Enter Your last name: "
+# last=gets.chomp
+
+# puts "Hello #{last} #{first}"
+
+#9. Write a Ruby program to check three numbers and return true if one or more of them are small. A number is called "small" if it is in the range 1..10 inclusive.
+
+# puts "Enter three no. :"
+# a=gets.chomp.to_i
+# b=gets.chomp.to_i
+# c=gets.chomp.to_i
+
+# if (a <= 10 || b <= 10 || c <= 10)
+#   puts "The number is small"
+# else
+#    puts "The numbers are greator than 10" 
+# end
+
+
+
